fbc38ebdd140f88c4b39d1603dd6a5ee54c75146
Thiennguyen0124 <thien.php.igb.vn>
2026-08-13 02:50:31 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use App\Http\Controllers\Cms\VariantGroupController;
 use App\Http\Controllers\Cms\VariantOptionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => view('welcome'));
+Route::redirect('/', '/cms');
 
 Route::prefix('cms')->name('cms.')->group(function () {
     Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
